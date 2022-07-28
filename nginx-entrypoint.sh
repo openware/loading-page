2758ee9968b75c96726f363f5fb5dddd03926670
philanton <filonenko100075@gmail.com>
2022-07-28 16:58:14 +0300

    feat: add base path

--- nginx-entrypoint.sh
+++ nginx-entrypoint.sh
@@ -6,4 +6,7 @@ INJECT_FILE_DST="${WWW_DIR}/inject.js"
 
 envsubst < "${INJECT_FILE_SRC}" > "${INJECT_FILE_DST}"
 
+mkdir -p ${WWW_DIR}/${BASE_PATH}
+cp -r ${WWW_DIR}/* ${WWW_DIR}/${BASE_PATH}
+
 [ -z "$@" ] && nginx -g 'daemon off;' || $@
